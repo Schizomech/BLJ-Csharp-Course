81f0bd6a829804429366d0d28ea227b6a347ddc6
romeo <romeo.arisona@gmail.com>
2025-10-01 09:40:07 +0200

    Finished Nr.15 Tannenbaum, Proceeding to creating random christmas bulbs on the tree.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie Breit soll der Baumstamm sein?");
+            int breiteB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wie hoch soll der Baumstamm sein?");
+            int hoheB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wie hoch soll die Krone sein?");
+            int hoheK = Convert.ToInt32(Console.ReadLine());
+            int count = 1;
+            int tool = hoheK - breiteB / 2;
+            Random rnd = new Random();
+
+            for (int lines = hoheK; lines >= 1; lines--)
+            {
+
+                for (int spaces = lines - 1; spaces >= 1; spaces--)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int star = 1; star <= count; star++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+
+                }
+                count++;
+
+                Console.WriteLine();
+            }
+            if (hoheB > 0 || breiteB > 0)
+            {
+                for (int hohe = 0; hohe < hoheB; hohe++)
+                {
+                    for (int center = 0; center < tool; center++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int breite = 0; breite < breiteB; breite++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
